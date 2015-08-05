6632a022bc8d9621ed7ed6f40a536c453436d1c3
John Huelsenbeck <johnh@berkeley.edu>
2015-08-05 15:15:48 -0700

    First commit

--- src/Settings.h
+++ src/Settings.h
@@ -7,48 +7,22 @@
 class Settings {
 
 	public:
-                            Settings(int argc, char *argv[]);
-				   double   getAsrvLambda(void) { return asrvLambda; }
-				   double   getBrlenLambda(void) { return brlenLambda; }
-				      int   getBurnIn(void) { return burnIn; }
-				      int   getChainLength(void) { return chainLength; }
-			  std::string   getDataFilePathName(void) { return dataFilePathName; }
-				      int   getPrintFrequency(void) { return printFrequency; }
-				      int   getNumGammaCats(void) { return numGammaCats; }
-			  std::string   getOutPutFileName(void) { return outPutFileName; }
-					  int   getSampleFrequency(void) { return sampleFrequency; }
-			  std::string   getTreeFileName(void) { return treeFileName; }
-					 void   setAsrvLambda(double x) { asrvLambda = x; }
-					 void   setBrlenLambda(double x) { brlenLambda = x; }
-					 void   setBurnIn(int x ) { burnIn = x; }
-					 void   setDataFilePathName(std::string s) { dataFilePathName = s; }
-					 void   setNumGammaCats(int x) { numGammaCats = x; }
-					 void   setOutPutFileName(std::string s) { outPutFileName = s; }
-					 void   setTreeFileName(std::string s ) { treeFileName = s; }
-					 bool   getIsConcFixed(void) { return isConcFixed; }
-				   double   getExpNumCats(void) { return expNumCats; }
-				   double   getPriorConcMean(void) { return priorConcMean; }
-				   double   getPriorConcVariance(void) { return priorConcVariance; }
-				   double   getTuningParm(std::string parmNameStr);
+                        Settings(int argc, char *argv[]);
+        double          getSpeciationRate(void) { return speciationRate; }
+        double          getExtinctionRate(void) { return extinctionRate; }
+        int             getNumLivingTaxa(void) { return numLivingTaxa; }
+        std::string     getOutputFileName(void) { return outputFileName; }
+        void            setSpeciationRate(double x) { speciationRate = x; }
+        void            setExtinctionRate(double x) { extinctionRate = x; }
+        void            setNumLivingTaxa(int x) { numLivingTaxa = x; }
+        void            setOutputFileName(std::string s) { outputFileName = s; }
 
-	private:
-	                 void   printUsage(void);
-				   double   brlenLambda;
-				      int   chainLength;
-					  int   burnIn;
-			  std::string   dataFilePathName;
-				      int   numGammaCats;
-			  std::string   outPutFileName;
-			  std::string   treeFileName;
-					  int   printFrequency;
-					  int   sampleFrequency;
-				   double   asrvLambda;
-				     bool   isConcFixed;
-				   double   expNumCats;
-				   double   priorConcMean;
-				   double   priorConcVariance;
-				   double   tuningParm[5];
-
+    private:
+        void            printUsage(void);
+        std::string     outputFileName;
+        double          speciationRate;
+        double          extinctionRate;
+        int             numLivingTaxa;
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "Settings.h"
 
 
 
 int main (int argc, char* argv[]) {
 
+    // get the user settings
+    Settings mySettings(argc, argv);
 
+    std::cout << "test" << std::endl;
     return 0;
 }
